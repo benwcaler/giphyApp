3d5131784119d14210d097cd798cdf36adc1c897
Ben <b.wayne@me.com>
2018-02-24 15:33:19 -0700

    bug with the URL not populating correctly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,33 @@
-//variables
+window.onload = function () {
+    //variables
+    var sTerm ;
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=B2biiO4oVNMtT2qJ2fAMgpgB5RgiuvS1&q=" + sTerm + "&limit=10&offset=0&rating=G&lang=en";
+    var buttons = ["Harry Potter", "Hermione Granger", "Ronald Weasley", "Severus Snape", "Albus Dumbledore", "Rubeus Hagrid", "Sirius Black", "He who shall not be named", "Minerva McGonagall", "Bellatrix Lestrange"];
+    //api calls
+    $(document).on("click", "#btn", function () {
+        sTerm = $(this).attr("data-name");
+        console.log(sTerm)
+        console.log($(this).attr("data-name"));
+        console.log(queryUrl)
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function (response) {
+            var gifSrc = response;
+            console.log(gifSrc)
+        })
+    })
+    //make the buttons
+    function btnLoad() {
+        for (var i = 0; i < buttons.length; i++) {
+            var btn = $("<button>");
+            btn.text(buttons[i]).attr("id", "btn");
+            btn.attr("data-name", buttons[i])
+            $("#click").append(btn)
+        }
+    }
+    btnLoad()
 
-//api calls
+    //add button function
 
-//make the buttons
-
-//make the form
-
-//post the results from the api
+}
