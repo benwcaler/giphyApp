d45e92a43c56c29ebb52ba765fb89d6159ca6b17
Ben <b.wayne@me.com>
2018-02-24 17:27:17 -0700

    just need to finish css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,60 @@
 window.onload = function () {
     //variables
-    var sTerm ;
-    var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=B2biiO4oVNMtT2qJ2fAMgpgB5RgiuvS1&q=" + sTerm + "&limit=10&offset=0&rating=G&lang=en";
-    var buttons = ["Harry Potter", "Hermione Granger", "Ronald Weasley", "Severus Snape", "Albus Dumbledore", "Rubeus Hagrid", "Sirius Black", "He who shall not be named", "Minerva McGonagall", "Bellatrix Lestrange"];
+    var topics = ["Harry Potter", "Hermione Granger", "Ronald Weasley", "Severus Snape", "Albus Dumbledore", "Rubeus Hagrid", "Sirius Black", "Tom Riddle", "Minerva McGonagall", "Bellatrix Lestrange"];
     //api calls
     $(document).on("click", "#btn", function () {
-        sTerm = $(this).attr("data-name");
-        console.log(sTerm)
-        console.log($(this).attr("data-name"));
-        console.log(queryUrl)
+        $("#gifs").empty();
+        var sTerm = $(this).val();
+        var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=B2biiO4oVNMtT2qJ2fAMgpgB5RgiuvS1&q=" + sTerm + "&limit=10&offset=0&rating=PG&lang=en";
         $.ajax({
             url: queryUrl,
             method: "GET"
         }).then(function (response) {
-            var gifSrc = response;
-            console.log(gifSrc)
+            var gifSrc = response.data;
+            for (var i = 0; i < gifSrc.length; i++) {
+                var div = $("<div>");
+                var img = $("<img>");
+                img.attr("src", gifSrc[i].images.fixed_height_still.url);
+                img.attr("data-still", gifSrc[i].images.fixed_height_still.url);
+                img.attr("data-animate", gifSrc[i].images.fixed_height.url);
+                img.attr("data-state", "still");
+                img.addClass("gif");
+                div.addClass("gifdiv")
+                div.append(img)
+                div.append("<p>" + "Rating: " + gifSrc[i].rating + "</p>");
+                $("#gifs").append(div);
+            }
         })
     })
     //make the buttons
     function btnLoad() {
-        for (var i = 0; i < buttons.length; i++) {
+        $("#click").empty()
+        for (var i = 0; i < topics.length; i++) {
             var btn = $("<button>");
-            btn.text(buttons[i]).attr("id", "btn");
-            btn.attr("data-name", buttons[i])
+            btn.text(topics[i]).attr("id", "btn");
+            btn.attr("value", topics[i])
             $("#click").append(btn)
         }
     }
     btnLoad()
 
-    //add button function
+    //animate images
+    $(document).on("click", ".gif", function () {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate")
+        } else if (state === "animate") {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    })
 
+    //add button function
+    $("#searchbtn").on("click", function(event) {
+        event.preventDefault();
+        topics.push($("#term").val())
+        $("#term").val("")
+        btnLoad()
+    });
 }
